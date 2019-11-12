dfb3d5ebfd2e6cc332eb1202a312591c65b08966
morenoloza98 <morenolozalex98@gmail.com>
2019-11-11 18:11:46 -0600

    File read
    File read

--- FileRead.java
+++ FileRead.java
@@ -1,3 +1,32 @@
+/**
+* This class is for reading a text file
+* @author  Alejandro Moreno Loza - A01654319
+* @author  Fabricio Andre Fuentes Fuentes - A01338527
+*/
+
+import java.util.*;
+import java.io.*;
+
 public class FileRead{
-	
+	public void readFile(File file){
+		List<String> list = new LinkedList<>();
+
+    	try{
+    		BufferedReader br = new BufferedReader(new FileReader(file));
+    		String st;
+			while ((st = br.readLine()) != null){
+		      list.add(st);
+		    }
+    	}catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		printFile(list);
+	}
+
+	public void printFile(List<String> fileDivided){
+		for (int i = 0; i < fileDivided.size(); i++) {
+			System.out.println(fileDivided.get(i));
+		}
+	}
 }
--- Main.java
+++ Main.java
@@ -1,7 +1,11 @@
+import java.util.*;
+import java.io.*; 
 
 public class Main{
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws Exception{
+		FileRead fileRead = new FileRead();
+		File file = new File("test1.txt");
+		fileRead.readFile(file);
 	}
 }
