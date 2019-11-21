42c6be598f96be8345c7ef9ef80c88608bbe8df8
fax117 <38862497+fax117@users.noreply.github.com>
2019-11-21 08:00:19 -0600

    GUI
    GUI vergas

--- Main.java
+++ Main.java
@@ -11,47 +11,58 @@ public class Main extends JFrame implements ActionListener{
 	
 	static String input;
 	static File selectedFile;
+	int width = 480;
+	int height = 720;
     JTextField inputTF;
 	JButton runPDA, chooseFile;
-	JLabel titleLabel, authorA, authorB, inputL;
+	static JLabel titleLabel, authorA, authorB, inputL, outputL;
 	
 	public Main(){
 		titleLabel = new JLabel("Top Down Parsing");
-		titleLabel.setBounds(150,-30,400,100);
+		titleLabel.setBounds(0,0,400,100);
+		titleLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		titleLabel.setFont(new Font("Arial", Font.ITALIC, 18));
-		titleLabel.setForeground(new Color(255,255,255));
-		authorA = new JLabel("Alejandro Moreno");
-		authorA.setBounds(30,0,400,100);
-		authorA.setForeground(new Color(255,255,255));
-		authorB = new JLabel("Fabricio Fuentes");
-		authorB.setBounds(150,0,400,100);
-		authorB.setForeground(new Color(255,255,255));
+		titleLabel.setForeground(new Color(0,0,0));
 
-		inputL = new JLabel("String to Process");
-		inputL.setBounds(30,60,400,100);
-		inputL.setForeground(Color.white);
+		authorA = new JLabel("Alejandro Moreno", SwingConstants.CENTER);
+		authorA.setBounds(-100,50,400,100);
+		authorA.setForeground(new Color(0,0,0));
+
+		authorB = new JLabel("Fabricio Fuentes", SwingConstants.CENTER);
+		authorB.setBounds(100,50,400,100);
+		authorB.setForeground(new Color(0,0,0));
+
+		inputL = new JLabel("String to Process", SwingConstants.CENTER);
+		inputL.setBounds(0,100,400,100);
+		inputL.setForeground(new Color(0,0,0));
+
 		inputTF = new JTextField();
-		inputTF.setBounds(30,120,50,20);
+		inputTF.setBounds(180,170,50,20);
 
-		runPDA = new JButton("Run");
-		runPDA.setBounds(130,390,120,50);
+		outputL = new JLabel("Result", SwingConstants.CENTER);
+		outputL.setBounds(0,180,400,100);
+		outputL.setForeground(new Color(0,0,0));
 
 		chooseFile = new JButton("Choose File");
-		chooseFile.setBounds(130,250,120,50);
+		chooseFile.setBounds(140,300,120,50);
 
-		setContentPane(new JLabel(new ImageIcon("img/Wall1.png")));
+		runPDA = new JButton("Run");
+		runPDA.setBounds(140,360,120,50);
+
+		setContentPane(new JLabel(new ImageIcon("img/Sky.jpg")));
 
 		add(titleLabel);
 		add(authorA);
 		add(authorB);
 		add(inputL);
+		add(outputL);
 		add(inputTF);
 		add(runPDA);
 		add(chooseFile);
 
 		runPDA.addActionListener(this);
 		chooseFile.addActionListener(this);
-		setSize(400,500);
+		setSize(width,height);
 		setLayout(null);
 		setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -60,15 +71,11 @@ public class Main extends JFrame implements ActionListener{
 	public void actionPerformed(ActionEvent evt){
         if(evt.getSource() == runPDA){
 			input = inputTF.getText();
-			if(input == null || input == "")
-				System.out.println("Please insert a valid string");
-			else
-				pushDown(input);
+			pushDown(input);
 		}
 		
-		if(evt.getSource() == chooseFile){
+		if(evt.getSource() == chooseFile)
 			ChooseFile();
-		}
 	}
 	
 	public static void main(String[] args){	
@@ -86,18 +93,28 @@ public class Main extends JFrame implements ActionListener{
 	}
 
 	public static void pushDown(String input){
+		System.out.println(input);
 		FileRead fileRead = new FileRead();
 		java.util.List<String> fileToSplit = new LinkedList<String>();
-		// File file = new File("test1.txt");
+
 		if(selectedFile == null)
-			System.out.println("Please Select a file with the grammar");
+			outputL.setText("Please Select a file with the grammar");
 		else{
 			fileToSplit = fileRead.readFile(selectedFile);
 			PDA pda = new PDA();
 			pda.splitFile(fileToSplit);
-			System.out.println(pda.topDown(input));
-			//pda.topDown("abba");
-		}
+			//System.out.println(pda.topDown(input));
 
+			if(input.equals(null) || input.equals("") || input.equals(" "))
+				outputL.setText("Please insert a valid string");
+			else{
+				boolean check = pda.topDown(input);
+
+				if(check)
+					outputL.setText("String belongs to the grammar");
+				else
+					outputL.setText("String does not belong to the grammar");
+			}
+		}
 	}
 }
